3cf806f169fdd434bf1e094cac449957dda17fe2
Ricardo Franco <ricardofrancosantos@gmail.com>
2025-04-15 07:16:25 -0300

    Classes

--- Javascript.js
+++ Javascript.js
@@ -1,5 +1,19 @@
-const myArrowFunction = (a, b) => {
-    return a + b
+class Product {
+    constructor(name,price) {
+        this.name=name
+        this.price=price
+    }
+
+    productDetails() {
+        return `O nome do produto é ${this.name} e o preço é ${this.price}`
+    }
 }
 
-console.log(myArrowFunction(2,5))
+const socks = new Product ('Meia branca', 10.99)
+const shirt = new Product ('Camisa preta', 22.99)
+
+console.log(socks.name)
+console.log(socks.price)
+console.log(shirt.name)
+
+console.log(shirt.productDetails)
