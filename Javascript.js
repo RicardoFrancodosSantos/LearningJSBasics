075277c7c0e444cd4974cdf2345d9d925e2afde7
Ricardo Franco <ricardofrancosantos@gmail.com>
2025-04-14 22:16:10 -0300

    while and foor loops learning

---
+++ Javascript.js
@@ -0,0 +1,14 @@
+
+const myList = [1, 2, 3, 4, 5]
+let counter = 0
+
+while (counter < myList.length) {
+ console.log('Imprimindo: ' + myList[counter])
+ counter++
+}
+
+const mySecondList = ['a', 'b', 'c', 'd', 'e']
+
+for (let counter = 0; counter < mySecondList.length; counter++) {
+    console.log('Imprimindo:  ${mySecondList[counter]}')
+}
